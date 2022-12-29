5476f43b674f3e504bb030ab138a4d285a20e5da
Kristen Morgan <kristen@instacart.com>
2022-12-29 02:30:35 -0500

    day 5

--- 2019/day5.py
+++ 2019/day5.py
@@ -15,34 +15,42 @@ def get_value(line, val, mode):
     return line[val] if mode == 0 else val
 
 
-def part_1(line):
+def main(line, part=None):
     line = list(map(int, line.split(",")))
 
     i = 0
     while i < len(line):
         prog = str(line[i]).zfill(5)
-        ma, mb, mc, op = prog[0], prog[1], prog[2], prog[3:5]
+        ma, my, mx, op = list(map(int, [prog[0], prog[1], prog[2], prog[3:5]]))
 
-        if OPS_CODES.get(int(op)):
+        if OPS_CODES.get(op):
             x, y, z = line[i+1:i+4]
-            line[z] = OPS_CODES[int(op)](
-                get_value(line, x, mc), get_value(line, y, mb)
-            )
+            line[z] = OPS_CODES[op](get_value(line, x, mx), get_value(line, y, my))
             i += 4
-        elif int(op) == 3:
-            line[line[i+1]] = 1
+        elif op == 3:
+            line[line[i+1]] = 1 if part == 1 else 5
             i += 2
-        elif int(op) == 4:
-            print(line[line[i+1]])
+        elif op == 4:
+            print(get_value(line, line[i+1], mx))
             i += 2
-        elif int(op) == 99:
+        elif op == 5:
+            x, y = line[i+1:i+3]
+            i = get_value(line, y, my) if get_value(line, x, mx) != 0 else i + 3
+        elif op == 6:
+            x, y = line[i+1:i+3]
+            i = get_value(line, y, my) if get_value(line, x, mx) == 0 else i + 3
+        elif op == 7:
+            x, y, z = line[i+1:i+4]
+            line[z] = 1 if get_value(line, x, mx) < get_value(line, y, my) else 0
+            i += 4
+        elif op == 8:
+            x, y, z = line[i+1:i+4]
+            line[z] = 1 if get_value(line, x, mx) == get_value(line, y, my) else 0
+            i += 4
+        elif op == 99:
             break
 
 
-def main(data, part=None):
-    part_1(data[0])
-
-
 if __name__ == '__main__':
-    print(f'Part 1 {main(lines, 1)}')
-    # print(f'Part 2 {main(lines, 2)}')
+    print(f'Part 1 {main(lines[0], 1)}')
+    print(f'Part 2 {main(lines[0], 2)}')
