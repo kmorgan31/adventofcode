19ac21f711c8b9f85b87a2afbf96d890832c0d23
Kristen Morgan <kristen@instacart.com>
2023-01-10 23:30:06 -0500

    day 7

--- 2016/day7.py
+++ 2016/day7.py
@@ -5,13 +5,6 @@ from aocd import lines
 import re
 from itertools import product
 
-EXAMPLE = [
-    "aaaaa-bbb-z-y-x-123[abxyz]",
-    "a-b-c-d-e-f-g-h-987[abcde]",
-    "not-a-real-room-404[oarel]",
-    "totally-real-room-200[decoy]"
-]
-
 
 def parse_line(line):
     line = re.split(r'\[([^\]]+)\]', line)
